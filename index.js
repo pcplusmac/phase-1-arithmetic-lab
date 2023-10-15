7e79cfd9b6ce1234593fc1445e66777749cb55f7
el-sam <samurai.h@yahoo.com>
2023-10-15 14:36:17 +0800

    pass 1st test, num1 and num2 multply to equal to 62

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+// declare num1 and num2
+let num1 = random();
+let num2 = random();
+
+// declare multiple and assign expected value to it. 
+const multiply = 62;
+
+if (multiply === num1 * num2) {
+    console.log("the result is 62")
+};
+
+
+function random () {
+    let x = Math.floor(Math.random() * 100) + 1;
+    return x;
+}
+
+
+
+
