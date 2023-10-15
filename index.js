5d1ea94ad8a7185653e6fa83720e8525fc0e5fef
el-sam <samurai.h@yahoo.com>
2023-10-15 14:57:33 +0800

    pass 3rd test; mod is the equation of restult 4

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 // Write your code here
-// declare num1 and num2
+// declare numbers
 let num1 = random();
 let num2 = random();
+let num3 = random();
+let num4 = random();
 
 // declare multiple and assign expected value to it. 
 const multiply = 62;
@@ -13,6 +15,10 @@ if (multiply === num1 * num2) {
 
 // declare a variable 'random' store the random number greater than 0
 let random = Math.floor(Math.random() * 100) + 1;
+
+//Create a variable called mod .
+const mod = 4;
+num3%num4 === mod ? console.log ("yes") : console.log("  ... ");
 
 function random () {
     let x = Math.floor(Math.random() * 100) + 1;
